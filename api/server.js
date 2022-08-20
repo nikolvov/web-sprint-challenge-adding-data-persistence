8381bd3f2eae2bf1d026189f7e54428351823753
Nikolai Lvov <nikolailvov@Nikolais-MacBook-Pro.local>
2022-08-19 20:53:23 -0700

    Index and server connected

--- api/server.js
+++ api/server.js
@@ -1,1 +1,11 @@
 // build your server here and require it from index.js
+const express = require('express');
+const tasksRouter = require('./task/router');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/tasks', tasksRouter);
+
+module.exports = server;

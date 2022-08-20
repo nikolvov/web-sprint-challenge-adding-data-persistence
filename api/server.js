2d30115b49637129a377cb45523fb23250ab23b8
Nikolai Lvov <nikolailvov@Nikolais-MacBook-Pro.local>
2022-08-19 20:59:35 -0700

    connected routers to server

--- api/server.js
+++ api/server.js
@@ -1,5 +1,8 @@
 // build your server here and require it from index.js
 const express = require('express');
+
+const projectsRouter = require('./project/router');
+const resourcesRouter = require('./resource/router');
 const tasksRouter = require('./task/router');
 
 const server = express();
@@ -7,5 +10,7 @@ const server = express();
 server.use(express.json());
 
 server.use('/api/tasks', tasksRouter);
+server.use('/api/resources', resourcesRouter);
+server.use('/api/projects', projectsRouter);
 
 module.exports = server;
